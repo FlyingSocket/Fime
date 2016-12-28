cd9f124aa8ab1ff08fe846e351604cfcccd50d47
Melvin SCHULTZ <mschultz@kaliop.com>
2016-12-28 11:22:31 -0500

    add isSignedIn function in rofile.vue

--- src/components/pages/Profile.vue
+++ src/components/pages/Profile.vue
@@ -17,15 +17,28 @@ export default {
     }
   },
   mounted: function () {
-    this.getUserProfile()
+    this.isSignedIn()
   },
   methods: {
+    isSignedIn: function () {
+      firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+          console.log(user)
+          console.log('User is signed in.')
+          this.getUserProfile()
+        } else {
+          console.log(user)
+          console.log('No user is signed in.')
+        }
+      })
+    },
     getUserProfile: function () {
       console.log('get user\'s profile ...')
       var user = firebase.auth().currentUser
 
       if (user != null) {
         this.email = user.email
+        console.log(user)
         console.log(this.email)
       } else {
         console.log('No user')
